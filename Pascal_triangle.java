6e7e62ee7fe55e18306a333eeff06faa75df6365
Sakthi Prasath M <52925340+thisissakthi@users.noreply.github.com>
2019-11-28 18:44:23 +0530

    Update Pascal_triangle.java

--- Pascal_triangle.java
+++ Pascal_triangle.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Pascal_triangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Enter any Numbers");
 		Scanner in=new Scanner(System.in);
 		int a=in.nextInt();
